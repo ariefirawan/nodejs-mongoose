dd9a27d0b85d7bfe0ad9ae673a5de1fa57ca5050
ariefirawan <ariefirawan@users.noreply.github.com>
2020-02-08 16:15:38 +0700

    menambahkan middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,6 +18,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, save: false}));
 
 app.use((req, res, next) => {
     User.findById("5e3ce176932de304680f8c07")
