01fd500d301e3f0b5a7fbf0c74743b6865ea7a33
ariefirawan <ariefirawan@users.noreply.github.com>
2020-02-09 14:26:58 +0700

    end of session and cookies

--- app.js
+++ app.js
@@ -4,24 +4,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new mongoDBStore({
+    uri: 'mongodb+srv://ariefirawant:dksq7mT60cz1Qdnj@cluster0-fv6bh.mongodb.net/shop',
+    collection: 'session'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, save: false}));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 
 app.use((req, res, next) => {
-    User.findById("5e3ce176932de304680f8c07")
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
     .then(user => {
         req.user = user;
         next();
@@ -31,10 +41,11 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://ariefirawant:dksq7mT60cz1Qdnj@cluster0-fv6bh.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://ariefirawant:dksq7mT60cz1Qdnj@cluster0-fv6bh.mongodb.net/shop')
     .then(result => {
         console.log('Connected!')
         User.findOne()
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,29 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
-    });
+        pageTitle: 'Login',
+        isAuthenticated: false
+    })
 };
 
 exports.postLogin = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login'
-    });
+        User.findById("5e3ce176932de304680f8c07")
+        .then(user => {
+            req.session.isLogIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/')
+            })
+        })
+        .catch(err => console.log(err))
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err)
+        res.redirect('/');
+    })
 };
